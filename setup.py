b9dfc4abd8b3f1ed378f69038a388ffc3d5ea863
angelmf97 <48300185+angelmf97@users.noreply.github.com>
2021-04-15 01:23:45 +0200

    Setup updated

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
 from distutils.core import setup
-import setuptools
+
 setup(name='PYT-SBI',
     version='1.0',
     description='PYT-SBI project',
     author='Mireia Codina Tobías, Ángel Monsalve Fernández, María Torralvo Márquez',
     url='https://github.com/angelmf97/PYT-project',
-    packages=setuptools.find_packages(where='.')    # Change this in case we finally put the packages inside a directory
+    packages=['PDIB'],
+    scripts=['PDIB/PDIB.py', 'PDIB/refine.py', 'PDIB/compute_energy.py', 'PDIB/process_input_files.py', 'PDIB/main_functions.py']
     )
