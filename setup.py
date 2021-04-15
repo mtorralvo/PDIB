2f6a27dc16b906a9b8db1f76e5fff85a48736002
angelmf97 <angelmf97@usal.es>
2021-04-15 22:19:13 +0200

    GUI added to the setup

--- setup.py
+++ setup.py
@@ -6,5 +6,5 @@ setup(name='PYT-SBI',
     author='Mireia Codina Tobías, Ángel Monsalve Fernández, María Torralvo Márquez',
     url='https://github.com/angelmf97/PYT-project',
     packages=['PDIB'],
-    scripts=['PDIB/PDIB.py', 'PDIB/refine.py', 'PDIB/compute_energy.py', 'PDIB/process_input_files.py', 'PDIB/main_functions.py']
+    scripts=['PDIB/PDIB.py', 'PDIB/refine.py', 'PDIB/compute_energy.py', 'PDIB/process_input_files.py', 'PDIB/main_functions.py', 'PDIB/gui.py']
     )
